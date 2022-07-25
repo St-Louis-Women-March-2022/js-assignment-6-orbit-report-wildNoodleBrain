0efa46259d447642fad5145ab466c866309e0919
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-07-25 18:18:51 -0500

    modified:   src/app/orbit-counts/orbit-counts.component.html 	modified:   src/app/orbit-counts/orbit-counts.component.ts

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -2,30 +2,31 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+	selector: 'app-orbit-counts',
+	templateUrl: './orbit-counts.component.html',
+	styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
+	catergories: string[] = ['Total', 'Space Debris', 'Communication', 'Probe', 'Positioning', 'Space Station', 'Telescope'];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
-
-  count(value: string): number {
-	let count: number = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type.toLowerCase === value.toLowerCase) {
-			count += 1;
-		 }
-	  }
+	ngOnInit() {
 	}
-	return count;
- }
+
+	count(value: string): number {
+		let count: number = 0;
+		if (this.satellites) {
+			for (let i = 0; i < this.satellites.length; i++) {
+				if (this.satellites[i].type.toLowerCase === value.toLowerCase) {
+					count += 1;
+				}
+			}
+		}
+		return count;
+	}
 
 
 }
